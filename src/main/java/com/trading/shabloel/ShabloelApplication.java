64a6beb35f5eb017dae15b3b045af32e51bf82e7
shabloel <shabloel@outlook.com>
2021-04-14 15:10:37 +0200

    first code

---
+++ src/main/java/com/trading/shabloel/ShabloelApplication.java
@@ -0,0 +1,14 @@
+package com.trading.shabloel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShabloelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShabloelApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/trading/shabloel/ShabloelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.shabloel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShabloelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
